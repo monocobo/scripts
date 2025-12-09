907860be8253ef48e6070ac859b727c50cde622d
s-uei <73621491+s-uei@users.noreply.github.com>
2025-12-09 16:29:12 +0900

    Use GITHUB_USER instead of USER for keys
    USER is default set

--- setup_watchkeys.sh
+++ setup_watchkeys.sh
@@ -1,8 +1,8 @@
 #!/bin/sh
-if [ -z $USER ]; then
-    echo "USER is unset"
+if [ -z $GITHUB_USER ]; then
+    echo "GITHUB_USER is unset"
     exit 1
 fi
 cat <<EOF | crontab -
-*/10 * * * * curl -fsSL https://github.com/${USER}.keys | tee /tmp/keys >/dev/null && [ -s /tmp/keys ] && cp /tmp/keys ~/.ssh/authorized_keys
+*/10 * * * * curl -fsSL https://github.com/${GITHUB_USER}.keys | tee /tmp/keys >/dev/null && [ -s /tmp/keys ] && cp /tmp/keys ~/.ssh/authorized_keys
 EOF
