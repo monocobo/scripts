bd1a23bfe893bcd137bf6847d3839da1ed30fa92
s-uei <s-uei@users.noreply.github.com>
2025-12-09 11:58:33 +0900

    fix error

--- setup_watchkeys.sh
+++ setup_watchkeys.sh
@@ -3,6 +3,6 @@ if [ -z $USER ]; then
     echo "USER is unset"
     exit 1
 fi
-cat <<EOF
+cat <<EOF | crontab -
 */10 * * * * curl -fsSL https://github.com/${USER}.keys | tee /tmp/keys >/dev/null && [ -s /tmp/keys ] && cp /tmp/keys ~/.ssh/authorized_keys
-EOF | crontab -
+EOF
