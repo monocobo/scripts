6c8f3fd63739c5cc63b09377b3122314ad165541
s-uei <73621491+s-uei@users.noreply.github.com>
2025-12-16 11:52:51 +0900

    fix: ansible warn

--- setup_ssh.sh
+++ setup_ssh.sh
@@ -3,6 +3,7 @@ cat <<EOF > /etc/ssh/sshd_config
 PermitRootLogin no
 PasswordAuthentication no
 PubkeyAuthentication yes
+Subsystem sftp /usr/lib/openssh/sftp-server # for Ansible Warn
 EOF
 systemctl daemon-reload
 systemctl restart ssh
